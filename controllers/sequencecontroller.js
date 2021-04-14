2dc86de9a5642bdfe3fee0f36835ca74dfb3948a
aligendza <73717510+aligendza@users.noreply.github.com>
2021-04-13 22:01:44 -0400

    join table working

--- controllers/sequencecontroller.js
+++ controllers/sequencecontroller.js
@@ -2,6 +2,7 @@ let express = require('express');
 const router = express.Router();
 let validateSession = require('../middleware/validate-session');
 const Sequence = require('../models/sequence');
+const SequencePose = require('../models/sequencePose');
 
 ///CREATE
 router.post('/create', validateSession, function (req, res) {
@@ -38,17 +39,42 @@ router.post('/create', validateSession, function (req, res) {
 // });
 
 ///ADD A POSE 
-router.put('/addpose/:id', validateSession, function (req, res) {  
+router.put('/addpose/:sequenceId/:poseId', validateSession, function (req, res) {  
     const addPose = {
-        title: req.body.title,
+        // title: req.body.title,
         posesInSequence: req.body.posesInSequence,
     };
     // const query = { where: { id: req.params.entryId, owner: req.user.id } };
     const query = { where: { id: req.params.id } };
-    Sequence.update(addPose, query)
-        .then((sequences) => res.status(200).json(sequences)) 
-        .catch((err) => res.status(500).json({ error: err }))
+    // Sequence.update(addPose, query)
+    //     .then((sequences) => res.status(200).json(sequences)) 
+    //     .catch((err) => res.status(500).json({ error: err }))
+    SequencePose.create({sequenceId:req.params.sequenceId, poseId:req.params.poseId}
+        ).then(result=>res.json(result))
 });
+
+// add a pose 2 - attempting
+// router.post("/addpose", validateSession, function(req, res) {
+//     const addPose = {
+//         title: req.body.title,
+//         posesInSequence: req.body.posesInSequence,
+//     }
+//     const query = {
+//         where: {
+//             id: req.params.entryId,
+//             owner: req.user.id
+//         }
+//     }
+//     Sequence.create(addPose)
+//     .then((pose) =>
+//     res.status(200).json({
+//         pose: pose
+//     })).catch((err) =>
+//     res.status(500).json({
+//         error: err,
+//     }))
+
+// })
 
 //DELETE SEQUENCE ENTRY
 router.delete('/delete/:id', validateSession, function (req,res) {
