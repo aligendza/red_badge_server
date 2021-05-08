b87fb51c1c9e3a52ad781642088a1348f69923e9
aligendza <73717510+aligendza@users.noreply.github.com>
2021-05-08 16:39:18 -0400

    find one get added

--- controllers/posecontroller.js
+++ controllers/posecontroller.js
@@ -1,63 +1,64 @@
-let express = require('express');
+let express = require("express");
 const router = express.Router();
-let validateSession = require('../middleware/validate-session');
-const Pose = require ('../models/pose');
+let validateSession = require("../middleware/validate-session");
+const Pose = require("../models/pose");
 ///CREATE
-router.post('/create', validateSession, function (req, res) {
-    // router.post('/create', function (req, res) {
-   Pose.create({
-        // owner: req.user.id,
-        nameEng: req.body.nameEng,
-        nameSans: req.body.nameSans,
-        imgUrl: req.body.imgUrl,
-        poseCat: req.body.poseCat,
-       
+router.post("/create", validateSession, function (req, res) {
+  // router.post('/create', function (req, res) {
+  Pose.create({
+    // owner: req.user.id,
+    nameEng: req.body.nameEng,
+    nameSans: req.body.nameSans,
+    imgUrl: req.body.imgUrl,
+    poseCat: req.body.poseCat,
+  })
+    .then(function createSuccess(pose) {
+      res.json({
+        pose: pose,
+        message: "Pose added",
+      });
     })
-        .then(
-            function createSuccess(pose) {
- 
-                res.json({
-                    pose: pose,
-                    message: 'Pose added',
-                })
-            }
-        )
-        .catch(err => res.status(500).json({ error: err }))
+    .catch((err) => res.status(500).json({ error: err }));
 });
 
-///UPDATE 
-router.put('/update/:entryId', validateSession, function (req, res) {  
-    const updatePose = {
-        nameEng: req.body.nameEng,
-        nameSans: req.body.nameSans,
-        imgUrl: req.body.imgUrl,
-        poseCat: req.body.poseCat,
-    };
+///UPDATE
+router.put("/update/:entryId", validateSession, function (req, res) {
+  const updatePose = {
+    nameEng: req.body.nameEng,
+    nameSans: req.body.nameSans,
+    imgUrl: req.body.imgUrl,
+    poseCat: req.body.poseCat,
+  };
 
-    const query = { where: { id: req.params.entryId, owner: req.user.id } };
-    Pose.update(updatePose, query)
-        .then((poses) => res.status(200).json(poses)) 
-        .catch((err) => res.status(500).json({ error: err }))
+  const query = { where: { id: req.params.entryId, owner: req.user.id } };
+  Pose.update(updatePose, query)
+    .then((poses) => res.status(200).json(poses))
+    .catch((err) => res.status(500).json({ error: err }));
 });
-
 
 //DELETE POSE ENTRY
-router.delete('/delete/:id', validateSession, function (req,res) {
-    const query = {where: {id: req.params.id}};
-    Pose.destroy(query)
-    .then(() => res.status(200).json({ message: 'Pose Removed'}))
-    .catch((err) => res.status(500).json({error: err}))
+router.delete("/delete/:id", validateSession, function (req, res) {
+  const query = { where: { id: req.params.id } };
+  Pose.destroy(query)
+    .then(() => res.status(200).json({ message: "Pose Removed" }))
+    .catch((err) => res.status(500).json({ error: err }));
 });
-
 
 //FIND ALL POSES BY USER
-router.get('/', validateSession, (req, res) => {
-    Pose.findAll({
-        // where: {standing: true}
-    })
-        .then(poses => res.status(200).json(poses))
-        .catch(err => res.status(500).json({ error: err }))
+router.get("/", validateSession, (req, res) => {
+  Pose.findAll({
+    // where: {standing: true}
+  })
+    .then((poses) => res.status(200).json(poses))
+    .catch((err) => res.status(500).json({ error: err }));
 });
 
+router.get("/one/:id", validateSession, (req, res) => {
+  Pose.findOne({
+    where: { id: req.params.id },
+  })
+    .then((poses) => res.status(200).json(poses))
+    .catch((err) => res.status(500).json({ error: err }));
+});
 
 module.exports = router;
